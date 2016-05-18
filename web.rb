7e2e63eabdf75b3a5b3a770951813f85650b9ebc
dyjakan <dyjakan@gmail.com>
2016-05-18 22:07:08 +0100

    Switching HTML to JSON.

--- web.rb
+++ web.rb
@@ -1,19 +1,23 @@
 require 'sinatra'
 require 'httparty'
 require 'nokogiri'
+require 'json'
 
 get '/' do
   resp = HTTParty.get("http://bh.buscms.com/api/REST/html/departureboard.aspx?callback=BusCms.widgets[%27widgetLookupDepartures_stop-departureboardCanvas%27].loadStopTimes_callback&_=1463155378395&clientid=BrightonBuses&sourcetype=siri&stopid=7731&format=jsonp&servicenamefilter=")
   html = resp[91..-4]
   html.gsub!("\\\"", "\"")
 
-  output = ""
-  Nokogiri::HTML(html).css("tr[class='rowServiceDeparture']").each do |row|
-    output += "<p>" + row.css("td[class='colServiceName']").text
-    output += " (-> " + row.css("td")[1]["title"] + ")"
-    output += ": "
-    output += row.css("td[data-departuretime]").text
-    output += " (" + row.css("td")[2]["data-departuretime"] + ")" + "</p>"
+
+  output = {response_type: "in_channel"}
+  line = ""
+  Nokogiri::HTML(html).css("tr[class='rowServiceDeparture']").each_with_index do |row, i|
+    line += row.css("td[class='colServiceName']").text
+    line += " (" + row.css("td")[1]["title"] + ")"
+    line += ": "
+    line += row.css("td[data-departuretime]").text
+    line += " (" + row.css("td")[2]["data-departuretime"] + ")"
+    output[i.to_s] = line
   end
-  output
+  output.to_json
 end
