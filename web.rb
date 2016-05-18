710afebfe780435ca9d52500fb447ea4c8f2b261
dyjakan <dyjakan@gmail.com>
2016-05-18 22:20:52 +0100

    Make JSON comaptible with slackbot

--- web.rb
+++ web.rb
@@ -3,7 +3,7 @@ require 'httparty'
 require 'nokogiri'
 require 'json'
 
-get '/' do
+# get '/' do
   resp = HTTParty.get("http://bh.buscms.com/api/REST/html/departureboard.aspx?callback=BusCms.widgets[%27widgetLookupDepartures_stop-departureboardCanvas%27].loadStopTimes_callback&_=1463155378395&clientid=BrightonBuses&sourcetype=siri&stopid=7731&format=jsonp&servicenamefilter=")
   html = resp[91..-4]
   html.gsub!("\\\"", "\"")
@@ -11,13 +11,16 @@ get '/' do
 
   output = {response_type: "in_channel"}
   line = ""
+  attachments = []
   Nokogiri::HTML(html).css("tr[class='rowServiceDeparture']").each_with_index do |row, i|
     line += row.css("td[class='colServiceName']").text
     line += " (" + row.css("td")[1]["title"] + ")"
     line += ": "
     line += row.css("td[data-departuretime]").text
     line += " (" + row.css("td")[2]["data-departuretime"] + ")"
-    output[i.to_s] = line
+    attachments << {text: line}
+    line = ""
   end
-  output.to_json
+  output["attachments"] = attachments
+  puts output.to_json
 end
