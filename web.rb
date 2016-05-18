858ec03a7dc555ccb5746d5104034f5134529473
dyjakan <dyjakan@gmail.com>
2016-05-18 21:45:11 +0100

    HTMLify output.

--- web.rb
+++ web.rb
@@ -6,11 +6,14 @@ get '/' do
   resp = HTTParty.get("http://bh.buscms.com/api/REST/html/departureboard.aspx?callback=BusCms.widgets[%27widgetLookupDepartures_stop-departureboardCanvas%27].loadStopTimes_callback&_=1463155378395&clientid=BrightonBuses&sourcetype=siri&stopid=7731&format=jsonp&servicenamefilter=")
   html = resp[91..-4]
   html.gsub!("\\\"", "\"")
+
+  output = ""
   Nokogiri::HTML(html).css("tr[class='rowServiceDeparture']").each do |row|
-    print row.css("td[class='colServiceName']").text
-    print " (" + row.css("td")[1]["title"] + ")"
-    print ": "
-    print row.css("td[data-departuretime]").text
-    puts " (" + row.css("td")[2]["data-departuretime"] + ")"
+    output += "<p>" + row.css("td[class='colServiceName']").text
+    output += " (-> " + row.css("td")[1]["title"] + ")"
+    output += ": "
+    output += row.css("td[data-departuretime]").text
+    output += " (" + row.css("td")[2]["data-departuretime"] + ")" + "</p>"
   end
+  output
 end
