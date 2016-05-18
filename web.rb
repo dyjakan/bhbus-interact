349a6a399826bd9dcbf661367fea01dc07682fdb
dyjakan <dyjakan@gmail.com>
2016-05-18 22:22:45 +0100

    ooops

--- web.rb
+++ web.rb
@@ -3,7 +3,7 @@ require 'httparty'
 require 'nokogiri'
 require 'json'
 
-# get '/' do
+get '/' do
   resp = HTTParty.get("http://bh.buscms.com/api/REST/html/departureboard.aspx?callback=BusCms.widgets[%27widgetLookupDepartures_stop-departureboardCanvas%27].loadStopTimes_callback&_=1463155378395&clientid=BrightonBuses&sourcetype=siri&stopid=7731&format=jsonp&servicenamefilter=")
   html = resp[91..-4]
   html.gsub!("\\\"", "\"")
