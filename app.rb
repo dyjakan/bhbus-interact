a9d9d82c0b4fe864378132057cbeed6957dc6476
dyjakan <dyjakan@gmail.com>
2016-05-20 23:20:03 +0100

    Refactoring

--- app.rb
+++ app.rb
@@ -3,38 +3,39 @@ require 'httparty'
 require 'nokogiri'
 require 'json'
 
-get '/' do
-  resp = HTTParty.get("http://bh.buscms.com/api/REST/html/departureboard.aspx?callback=BusCms.widgets[%27widgetLookupDepartures_stop-departureboardCanvas%27].loadStopTimes_callback&_=&clientid=BrightonBuses&sourcetype=siri&stopid=7731&format=jsonp&servicenamefilter=")
-  html = resp[91..-4]
+get '/times/:name' do
+  base_url = "http://bh.buscms.com/api/REST/html/departureboard.aspx?callback=BusCms.widgets[%27widgetLookupDepartures_stop-departureboardCanvas%27].loadStopTimes_callback&_=&clientid=BrightonBuses&sourcetype=siri&stopid=7731&format=jsonp&servicenamefilter="
+  response = HTTParty.get(base_url)
+  html = response[91..-4]
   html.gsub!("\\\"", "\"")
 
-  buses = ""
+  bus_list = ""
   Nokogiri::HTML(html).css("tr[class='rowServiceDeparture']").each_with_index do |row, i|
-    buses += row.css("td[class='colServiceName']").text
-    buses += " (" + row.css("td")[1]["title"] + ")"
-    buses += ": "
-    buses += row.css("td[data-departuretime]").text
-    buses += " (" + row.css("td")[2]["data-departuretime"] + ")\n"
+    bus_list += row.css("td[class='colServiceName']").text
+    bus_list += " (" + row.css("td")[1]["title"] + ")"
+    bus_list += ": "
+    bus_list += row.css("td[data-departuretime]").text
+    bus_list += " (" + row.css("td")[2]["data-departuretime"] + ")\n"
   end
-  respond_with buses
+  respond_with bus_list
 end
 
-get '/search/:name' do
-  resp = search params['name'].gsub(" ", "+")
-  "#{resp}"
+get '/stops/:name' do
+  stops = get_stops params['name'].gsub(" ", "+")
+  "#{stops}"
 end
 
-def search stop_name
+def get_stops(name)
   base_url = "http://bh.buscms.com/api/rest/ent/stop.aspx?callback=jsonp1463771285418&clientid=BrightonBuses&method=search&format=jsonp&q="
-  HTTParty.get(base_url + stop_name)
+  HTTParty.get(base_url + name)
 end
 
-def respond_with message
+def respond_with(message)
   content_type :json
   {text: message}.to_json
 end
 
 error Sinatra::NotFound do
   content_type 'text/plain'
-  [404, 'Not Found']
+  [404, 'Method Not Found']
 end
